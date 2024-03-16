193a5eb01adbb2856277ac8c975de59a18f04162
Gwyn <gwyn@playtechnique.io>
2024-03-16 14:44:50 -0600

    Explicitly add nav element everywhere to assist with search crawler.

--- website/main.js
+++ website/main.js
@@ -1,20 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
-    header(function() {
+    addCodeHighlightingToHeader(function() {
         // Call hljs.highlightAll(); after highlight.js has loaded
         hljs.highlightAll();
     });
 });
 
-function header(callback) {
-    var isNotFrontPage = window.location.pathname !== "/"; // Check if the current page is not the front page
-    document.getElementById("header").innerHTML = `
-        <nav class="navigation">` +
-            (isNotFrontPage ? `<a href="/" class="link">front page</a> ` : "") + // Conditionally include front page link
-            `<a href="/articles/index.html" class="link">articles</a>
-            <a href="/projects/index.html" class="link">projects</a>
-        </nav>
-        
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css"/>`;
+function addCodeHighlightingToHeader(callback) {
+    document.getElementById("header").innerHTML = `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css"/>`;
 
     var script = document.createElement('script');
     script.src = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js";
