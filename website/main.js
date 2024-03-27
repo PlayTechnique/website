084fad213f5a04b388677e5ba315dc6a23785dea
Gwyn <gwyn@playtechnique.io>
2024-03-27 08:06:32 -0600

    Adds google analytics.

--- website/main.js
+++ website/main.js
@@ -16,6 +16,33 @@ function addCodeHighlightingToHeader(callback) {
     document.body.appendChild(script);
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+    addGoogleTagToHead();
+});
 
+document.addEventListener('DOMContentLoaded', function () {
+    addGoogleTagToHead();
+});
 
+function addGoogleTagToHead() {
+    // Create the script tag for gtag.js
+    var gtagScript = document.createElement('script');
+    gtagScript.async = true;
+    gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-B7K8ZDE6MK';
+    
+    // Append the gtag.js script to the <head>
+    document.head.appendChild(gtagScript);
 
+    // Inline script to initialize gtag
+    var inlineScript = document.createElement('script');
+    inlineScript.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-B7K8ZDE6MK');
+    `;
+    
+    // Append the inline script after the gtag.js script
+    document.head.appendChild(inlineScript);
+}
