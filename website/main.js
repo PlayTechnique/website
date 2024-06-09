4239412e9e59581500347ca69dddc832baaac49b
Gwyn <gwyn@playtechnique.io>
2024-06-09 13:43:52 -0600

    Updates confusingly-named header attribute to be playtechnique-header
    This ID was used before I realised there's a <header> element; it
    can be confusing to reason about two things named the same thing so this
    rename

--- website/main.js
+++ website/main.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function addCodeHighlightingToHeader(callback) {
-    document.getElementById("header").innerHTML = `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css"/>`;
+    document.getElementById("playtechnique-header").innerHTML = `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/default.min.css"/>`;
 
     var script = document.createElement('script');
     script.src = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js";
