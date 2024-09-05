a6db9e7b6cc151f82af7ead3885e1dfda76eeb99
Gwyn <gwyn@playtechnique.io>
2024-09-05 08:01:17 -0600

    New article: marching with two pointers.

--- content/main.js
+++ content/main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    addCodeHighlightingToHeader(function() {
+    addCodeHighlightingToHeader(function () {
         // Call hljs.highlightAll(); after highlight.js has loaded
         hljs.highlightAll();
     });
@@ -10,7 +10,7 @@ function addCodeHighlightingToHeader(callback) {
 
     var script = document.createElement('script');
     script.src = "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js";
-    script.onload = function() {
+    script.onload = function () {
         if (callback) callback(); // Call callback function when script is loaded
     };
     document.body.appendChild(script);
@@ -25,24 +25,29 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function addGoogleTagToHead() {
-    // Create the script tag for gtag.js
-    var gtagScript = document.createElement('script');
-    gtagScript.async = true;
-    gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-B7K8ZDE6MK';
-    
-    // Append the gtag.js script to the <head>
-    document.head.appendChild(gtagScript);
+    if (window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1') {
 
-    // Inline script to initialize gtag
-    var inlineScript = document.createElement('script');
-    inlineScript.innerHTML = `
+        // Create the script tag for gtag.js
+        var gtagScript = document.createElement('script');
+        gtagScript.async = true;
+        gtagScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-B7K8ZDE6MK';
+
+        // Append the gtag.js script to the <head>
+        document.head.appendChild(gtagScript);
+
+        // Inline script to initialize gtag
+        var inlineScript = document.createElement('script');
+        inlineScript.innerHTML = `
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
 
       gtag('config', 'G-B7K8ZDE6MK');
     `;
-    
-    // Append the inline script after the gtag.js script
-    document.head.appendChild(inlineScript);
+
+        // Append the inline script after the gtag.js script
+        document.head.appendChild(inlineScript);
+    } else {
+        console.log('Google Analytics is not loaded in local environment.');
+    }
 }
